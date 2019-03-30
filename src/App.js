0d646d9a4195d48c7cbc05f405e94f95dfed62a7
Mike <mfisher.uk@gmail.com>
2019-03-30 16:49:06 +0000

    Added basic styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import instagramLogo from './images/glyph-logo_May2016.png'
 import twitterLogo from './images/Twitter_Logo_Blue.png'
+import afftag from './images/afftag.png'
+import instagramtag from './images/instagramtag.png'
+import twittertag from './images/twittertag.png'
 import './App.css';
 
 class App extends Component {
@@ -98,11 +101,12 @@ class App extends Component {
 
   getMoreFeeds = async(req, res) => {
     console.log(this.state.feedData)
+    console.log(this.state.currentFilter)
     //var currentRecords = this.state.feedData;
     try{
       await fetch('http://private-cc77e-aff.apiary-mock.com/posts')
         .then(res => res.json())
-        .then(resitems => resitems.items.filter(tweet => tweet.service_name === this.state.currentFilter))    
+        .then(resitems => this.state.currentFilter !== "" ? resitems.items.filter(tweet => tweet.service_name === this.state.currentFilter) : resitems.items)    
         .then(moreFeeds => this.setState({feedData: [...this.state.feedData, ...moreFeeds]}, function(){console.log(this.state.feedData)}))        
     }
     catch{
@@ -137,14 +141,15 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+        <div className="mainmenu">
           <h3>Autumn Fashion Fix</h3>
 
-          <button onClick={() => this.getFeedData()}>All Feeds</button>
-          <button onClick={() => this.filterTweets()}>Filter Tweets</button>
-          <button onClick={() => this.filterManualFeeds()}>Filter Manual feeds</button>
-          <button onClick={() => this.filterInstagram()}>Filter Instagram</button>
+          <button onClick={() => this.getFeedData()} className={this.state.currentFilter === "" ? "active": ""}>All Feeds</button>
+          <button onClick={() => this.filterTweets()} className={this.state.currentFilter === "Twitter" ? "active": ""}>Filter Tweets</button>
+          <button onClick={() => this.filterManualFeeds()} className={this.state.currentFilter === "Manual" ? "active": ""}>Filter Manual feeds</button>
+          <button onClick={() => this.filterInstagram()} className={this.state.currentFilter === "Instagram" ? "active": ""}>Filter Instagram</button>
 
-          <form onSubmit={this.getFeedData}>
+          {/* <form onSubmit={this.getFeedData}>
             <label>
               <input type="radio" value="" checked={this.state.currentFilter === ""} onChange={this.getFeedData}/>
               All
@@ -161,56 +166,72 @@ class App extends Component {
               <input type="radio" value="Instagram" checked={this.state.currentFilter === "Instagram"} onChange={this.filterInstagram}/>
               Instagram
             </label>            
-          </form>          
+          </form> */}
+          </div>          
         </header>
 
 
         <div>
         <div className="imagecontainer">
+            <div className="socialwall">
             
             {this.state.feedData.map((item, index) =>{
               var userName = "",
                 bodytext = "",
                 imageTag = "",
+                tagicon = "",
                 tags = "",
                 link = ""
 
               if(item.service_name === "Instagram"){
-                bodytext = item.item_data.caption;                
+                bodytext = item.item_data.caption;  
+                tagicon = <img src={instagramtag} className="tagicon"/>;              
 
                 if(item.item_data.tags.length > 0){
                   tags = <p className="tags">{'#' + item.item_data.tags.join(' #')}</p>
                 }
 
-                imageTag = item.item_data.image.medium;
-                //imageTag = 'https://scontent-lht6-1.cdninstagram.com/vp/b8bcc77658eadac2016a94c39ed43c90/5D2F8FEF/t51.2885-15/e15/10598470_1480439188872772_1890435781_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com'
+                //imageTag = item.item_data.image.medium;
+                imageTag = 'https://scontent-lht6-1.cdninstagram.com/vp/b8bcc77658eadac2016a94c39ed43c90/5D2F8FEF/t51.2885-15/e15/10598470_1480439188872772_1890435781_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com'
               }
 
               if(item.service_name === "Twitter"){
-                userName = item.item_data.user.username;
+                userName = <h4>{item.item_data.user.username}</h4>;
                 bodytext = item.item_data.tweet;
+                tagicon = <img src={twittertag} className="tagicon"/>;
+                //bodytext = item.item_data.tweet.replace(/(^|\s)(#[a-z\d-]+)/ig, "$1<a href='https://twitter.com/hashtag/$2?src=hash' class='hash_tag'>$2</a>"); 
               }
 
               if(item.service_name === "Manual"){
                 bodytext = item.item_data.text;
                 imageTag = item.item_data.image_url;
+                tagicon = <img src={afftag} className="tagicon"/>;
                 link = <a href={item.item_data.link} target="_blank">{item.item_data.link_text}</a>
               }
 
               return(
-                <div className="imagecontent" key={index}>
-                <h4>Feedtype : {item.service_name}</h4>
-                  <h4>{userName}</h4>
-                  <img onError={this.addDefaultSrc.bind(this, item.service_name)} src={imageTag} />                  
+                <div className="tile" key={index}>
+                <div className="tilecontainer">
+                
+                {tagicon}
+                <div className="tilecontent">
+                  {/* <h4>Feedtype : {item.service_name}</h4> */}
+                  
+                  <img onError={this.addDefaultSrc.bind(this, item.service_name)} src={imageTag} />   
+                  {userName}               
                   <p dangerouslySetInnerHTML={createMarkup(bodytext)} />
                   {link}
                   {tags}
+                  </div>
+                </div>
+                
                   
                 </div>                    
               )
             })}                
         </div>
         <button onClick={() => this.getMoreFeeds()}>More</button>
+        </div>
         </div>
       </div>
     );
